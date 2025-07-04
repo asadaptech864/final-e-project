5634660f759273acec5f82f39b699d4991d20561
asadaptech864 <asadaptech864@gmail.com>
2025-07-04 12:43:52 +0500

    create backend and fronted

---
+++ Backend/index.mjs
@@ -0,0 +1,22 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import router from './Routes/routes.mjs'
+import cors from 'cors'
+const app = express()
+dotenv.config()
+app.use(express.json())
+app.use(cors())
+main().catch(err => console.log(err));
+const port = process.env.PORT
+
+async function main() {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log('Connected to MongoDB')
+}
+
+app.use('/',router)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
